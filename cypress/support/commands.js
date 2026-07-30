191c047de5cc0b9dce83a157d156b4035e3b34ff
fath-paper <fath.akbar@paper.id>
2026-07-30 10:45:16 +0700

    Add API request and response logging to commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,25 @@
-// Custom Cypress commands can be added here
-// Example: cy.login(username, password)
+Cypress.Commands.overwrite('request', (originalFn, ...options) => {
+  let body;
+  if (options.length === 1 && typeof options[0] === 'object') {
+    body = options[0].body;
+  } else if (options.length === 3) {
+    body = options[2];
+  }
 
-export {};
+  if (body) {
+    Cypress.log({
+      name: 'reqBody',
+      message: JSON.stringify(body)
+    });
+  }
+
+  return originalFn(...options).then((response) => {
+    if (response.body) {
+      Cypress.log({
+        name: 'resBody',
+        message: JSON.stringify(response.body)
+      });
+    }
+    return response;
+  });
+});
